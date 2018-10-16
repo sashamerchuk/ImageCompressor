b22023d3e0cf991a2d8ed102c24bda9a00daf63c
sashamerchuk <sashamerchuk16@gmail.com>
2018-10-16 23:02:45 +0100

    fetch method  not able on the chrome but able on the mozilla

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("appinit");
+
+fetch("./data/pictures.json")
+  .then(function(response) {
+    return response.json();
+   })
+  .then(function(compressedImage) {
+    console.log("pictures => ", compressedImage.pictures);
+  })
+  .catch(function(err) {
+      console.log("error happened => ", err);
+  });
