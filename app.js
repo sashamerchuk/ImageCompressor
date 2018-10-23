46d1035f9c85a525baf7f0fecefcd66d77ea70fe
sashamerchuk <sashamerchuk16@gmail.com>
2018-10-23 20:57:32 +0100

    перенесення даних об'єкту на штмл сторінку в таблицю

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-console.log("appinit");
-
-fetch("./data/pictures.json")
-  .then(function(response) {
-    return response.json();
-   })
-  .then(function(compressedImage) {
-    console.log("pictures => ", compressedImage.pictures);
+fetch('./data/pictures.json')
+  .then(function (response) {
+    return response.json()
+  }).then(function (json) {
+    console.log('parsed json', json)
+  }).catch(function (ex) {
+    console.log('parsing failed', ex)
   })
-  .catch(function(err) {
-      console.log("error happened => ", err);
-  });
+var pictures = {
+  name: "picture.jpg",
+  originalSize : "3mb",
+  compressedSize : "200kb",
+};
+var x = anyList;
+document.write(pictures.originalSize);
+var sasha = document.createElement("td");
+sasha.innerHTML = document.write(pictures.originalSize);
+x.insertBefore(sasha,x.children[1]);
+
+
+
 
