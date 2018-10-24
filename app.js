56e012de8a8b23af1ecc764af8a962f561f859bf
sashamerchuk <sashamerchuk16@gmail.com>
2018-10-24 21:56:03 +0100

    відобразив список картинок , всі дані вивів через обєкт

--- app.js
+++ app.js
@@ -1,22 +1,99 @@
-fetch('./data/pictures.json')
-  .then(function (response) {
-    return response.json()
-  }).then(function (json) {
-    console.log('parsed json', json)
-  }).catch(function (ex) {
-    console.log('parsing failed', ex)
-  })
-var pictures = {
-  name: "picture.jpg",
-  originalSize : "3mb",
-  compressedSize : "200kb",
+var picture = {
+    compressedSize: "200кб <strong>(-80%)</strong>",
+    originalSize: "1.2мб"
 };
-var x = anyList;
-document.write(pictures.originalSize);
-var sasha = document.createElement("td");
-sasha.innerHTML = document.write(pictures.originalSize);
-x.insertBefore(sasha,x.children[1]);
-
-
-
-
+var picture2 = {
+    compressedSize:"300kb <strong>(-90%)</strong>",
+    originalSize:"3mb"
+}
+var picture3 = {
+    compressedSize:"150kb <strong>-50%</strong>",
+    originalSize:"300kb"
+}
+var table = document.createElement("table"); // створюю таблицю
+table.className = "tbPictures"; //створюю клас
+pictures.appendChild(table); // додаю в div
+var tr = document.createElement("tr"); // create tr 
+tr.className = "trOrigin"; // created the class trPictures
+table.appendChild(tr);
+var td = document.createElement("td");//create td
+td.innerHTML = "";
+tr.appendChild(td);
+var tdO = document.createElement("td"); //create td element for original size
+tdO.innerHTML = "<strong>Original Size</strong>";
+tr.appendChild(tdO);
+var tdComp = document.createElement("td");
+tdComp.innerHTML = "<strong>Compressed Size</strong>";
+tr.appendChild(tdComp);
+var secondTR = document.createElement("tr");
+table.appendChild(secondTR);
+var tdImg = document.createElement("td");
+tdImg.innerHTML="";
+secondTR.appendChild(tdImg);
+var tdimg = document.createElement("img");// foto in td 
+tdimg.className = "fotoinTable";
+tdimg.src = "foto/church.jpg";
+tdImg.appendChild(tdimg);
+var tdDataobj = document.createElement("td");
+tdDataobj.innerHTML = picture.originalSize;
+secondTR.appendChild(tdDataobj);
+var tdDataobj1 = document.createElement("td");
+tdDataobj1.innerHTML = picture.compressedSize;
+secondTR.appendChild(tdDataobj1);
+var tdDataobj8 = document.createElement("td");
+tdDataobj8.innerHTML="";
+secondTR.appendChild(tdDataobj8);
+var tdDownload1 = document.createElement("a");
+tdDownload1.className="downloadInTable";
+tdDownload1.href = "foto/church.jpg";
+tdDownload1.download;
+tdDownload1.innerHTML="download";
+tdDataobj8.appendChild(tdDownload1); 
+var thirdTR = document.createElement("tr");
+table.appendChild(thirdTR);
+var tdImg2 = document.createElement("td");
+tdImg2.innerHTML="";
+thirdTR.appendChild(tdImg2);
+var tdimg2 = document.createElement("img");// foto in td 
+tdimg2.className = "fotoinTable";
+tdimg2.src = "foto/summer.jpg";
+tdImg2.appendChild(tdimg2);
+var tdDataobj2 = document.createElement("td");
+tdDataobj2.innerHTML=picture2.originalSize;
+thirdTR.appendChild(tdDataobj2);
+var tdDataobj3 = document.createElement("td");
+tdDataobj3.innerHTML = picture2.compressedSize;
+thirdTR.appendChild(tdDataobj3);
+var tdDataobj9 = document.createElement("td");
+tdDataobj9.innerHTML="";
+thirdTR.appendChild(tdDataobj9);
+var tdDownload2 = document.createElement("a");
+tdDownload2.className="downloadInTable";
+tdDownload2.href = "foto/summer.jpg";
+tdDownload2.download;
+tdDownload2.innerHTML="download";
+tdDataobj9.appendChild(tdDownload2); 
+var fourTR = document.createElement("tr");
+table.appendChild(fourTR);
+var tdDataobj4 = document.createElement("td");
+tdDataobj4.innerHTML="";
+fourTR.appendChild(tdDataobj4);
+var tdimg3 = document.createElement("img");// foto in td 
+tdimg3.className = "fotoinTable";
+tdimg3.src = "foto/sun.jpg";
+tdDataobj4.appendChild(tdimg3);
+var tdDataobj5 = document.createElement("td");
+tdDataobj5.innerHTML=picture3.originalSize;
+fourTR.appendChild(tdDataobj5);
+var tdDataobj6 = document.createElement("td");
+tdDataobj6.innerHTML = picture3.compressedSize;
+fourTR.appendChild(tdDataobj6);
+var tdDataobj7 = document.createElement("td");
+tdDataobj7.innerHTML="";
+fourTR.appendChild(tdDataobj7);
+var tdDownload3 = document.createElement("a");
+tdDownload3.className="downloadInTable";
+tdDownload3.href = "foto/sun.jpg";
+tdDownload3.download;
+tdDownload3.innerHTML="download";
+tdDataobj7.appendChild(tdDownload3); 
