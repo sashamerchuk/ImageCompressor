970f57496933f291189a08af2e53d6a54f34124d
Ostap Baranovskiy <ostap.baranovski@gmail.com>
2018-11-20 21:43:06 +0200

    limited photo and it size

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,41 +1,45 @@
-
-  
-
 export default class Upload {
   renderInput() {
     const input = document.createElement("input");
-    input.id="menu_images"
+    input.id = "menu_images";
     input.type = "file";
-    input.name = "image3";
-    input.setAttribute("multiple",true);
-    input.addEventListener("change",this.onchange);
-  
-return input;
+    input.name = "image";
+    input.setAttribute("multiple", true);
+    input.addEventListener("change", this.onchange);
+
+    return input;
   }
-  
 
   render() {
     const div = document.createElement("div");
     div.className = "upload";
-    div.appendChild(this.renderInput())
-    div.appendChild(this.renderImage())
-    
-    
-
-
-
-
+    div.appendChild(this.renderInput());
+    div.appendChild(this.renderImage());
     return div;
   }
   renderImage() {
     const image = document.createElement("img");
-    image.id="menu_image";
-    image.className="preview_img";
+    image.id = "menu_image";
+    image.className = "preview_img";
     return image;
   }
-  onchange (event){
-console.log(event.target.files)
-
+  onchange(event) {
+    console.log("Files arr:", event.target.files);
+    const sizeFile = event.target.files.length;
+    Array.from(event.target.files).forEach(file => {
+      console.log("File is:", file);
+      if (sizeFile > 10) {
+        const fileInput = document.getElementById("menu_images");
+        return (fileInput.disabled = true);
+      }
+      if (file.size > 5000);{
+        alert("you photo is too big");
+      }
+    });
   }
-  
 }
+
+
+
+
+  
